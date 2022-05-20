d43318b74a2eca4e6a81829148a9898858b243dd
unknown <s-touka99-mohamed@zewailcity.edu.eg>
2022-05-20 20:51:54 +0200

    added filtering lists in payment view

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: _title,
-      home:  PassengersView(), 
+      home:  PaymentView(), 
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/views/payment_view.dart
+++ lib/views/payment_view.dart
@@ -11,6 +11,30 @@ class _PaymentViewState extends State<PaymentView> {
   late double viewHeight;
   late double viewWidth;
 
+  String dropdownvalue = 'Route_ID 1';   
+  String dropdownvalue2= 'ID1';
+  String dropdownvalue_status= 'True';
+
+  // List of items in our dropdown menu
+  var routes_items = [    
+    'Route_ID 1',
+    'Route_ID 2',
+    'Route_ID 3',
+    'Route_ID 4',
+    'Route_ID 5',
+  ];
+
+  var ids_items = [    
+    'ID1',
+    'ID2',
+    'ID3',
+  ];
+
+  var status_items = [    
+    'True',
+    'False',
+  ];
+
   @override
   Widget build(BuildContext context) {
     screenHeight = MediaQuery.of(context).size.height;
@@ -21,20 +45,98 @@ class _PaymentViewState extends State<PaymentView> {
 
   Widget desktopView() {
     return Stack(children: [
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        body:
       Center(
         child: Column(
           children: <Widget>[
             Container(
               height: screenHeight * 0.05,
             ),
-            Container(
-              height: screenHeight * 0.1,
-              child: Center(child: Text('filters')),
+            SizedBox(
+                height: screenHeight * 0.1,
+                width: viewWidth*0.3,
+                child: 
+
+                Row(children: <Widget>[ 
+
+                const Text('Filter:     '),
+              
+              DropdownButton(
+              // Initial Value
+              value: dropdownvalue,
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),    
+              // Array list of items
+              items: routes_items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) { 
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
             ),
+
+            const Spacer(),
+            
+            DropdownButton(
+              // Initial Value
+              value: dropdownvalue2,
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),    
+              // Array list of items
+              items: ids_items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) { 
+                setState(() {
+                  dropdownvalue2 = newValue!;
+                });
+              },
+            ),
+
+            const Spacer(),
+            
+            DropdownButton(
+              // Initial Value
+              value: dropdownvalue_status,
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),    
+              // Array list of items
+              items: status_items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) { 
+                setState(() {
+                  dropdownvalue_status = newValue!;
+                });
+              },
+            )
+            ])),
+
+
+
             buildDatatable(),
           ],
         ),
-      ),
+      )),
     ]);
   }
 
