0cbade6412d65c6f0d91bdaa129a866326672d43
unknown <s-touka99-mohamed@zewailcity.edu.eg>
2022-05-20 20:39:33 +0200

    added filtering lists in passengers view

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gui/homepage.dart';
 import 'package:gui/login_page.dart';
 import 'package:gui/route_change_request_page.dart';
+import 'package:gui/views/passengers_view.dart';
+import 'package:gui/views/payment_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: _title,
-      home: const LoginPage(), 
+      home:  PassengersView(), 
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
